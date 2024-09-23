baa04965cdc4ae00a5de9fde89b07275a020ef0c
Guilherme Guia <guilhermeguia007@gmail.com>
2024-09-23 17:38:23 -0400

    feat: Adicionando entidade User

---
+++ src/DesafioPicPay.Domain/Entities/Enums/EUserType.cs
@@ -0,0 +1,7 @@
+﻿namespace DesafioPicPay.Domain.Entities.Enums;
+
+public enum EUserType
+{
+    Lojista,
+    Usuario
+}
---
+++ src/DesafioPicPay.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPicPay.Domain.Entities.Enums;
+using RecipeFood.Domain.Entities;
+
+namespace DesafioPicPay.Domain.Entities;
+
+public class User : EntityBase
+{
+    public string NomeCompleto { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Cpf { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public EUserType UserType { get; set; }
+}
