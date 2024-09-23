4d2a1a215eccb87746b2c5271563b3e35033c549
Guilherme Guia <guilhermeguia007@gmail.com>
2024-09-23 17:38:11 -0400

    feat: Adicionando entidade basica

---
+++ src/DesafioPicPay.Domain/Entities/EntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace RecipeFood.Domain.Entities;
+abstract public class EntityBase
+{
+    public long Id { get; set; }
+    public bool Active { get; set; } = true;
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+}
