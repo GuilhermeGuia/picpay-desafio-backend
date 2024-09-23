e6d1a3fa8bdd69b74256757c56309660da9f3979
Guilherme Guia <guilhermeguia007@gmail.com>
2024-09-23 17:37:44 -0400

    feat: Adicionando handle de exceções personalizadas

--- src/DesafioPicPay.API/Filters/ExceptionFilter.cs
+++ src/DesafioPicPay.API/Filters/ExceptionFilter.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using DesafioPicPay.API.Response;
+using DesafioPicPay.Exception.ExceptionBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace DesafioPicPay.API.Filters;
 
@@ -6,8 +9,27 @@ public class ExceptionFilter : IExceptionFilter
 {
     public void OnException(ExceptionContext context)
     {
-        
+        if(context.Exception is DesafioPicPayException)
+        {
+            HandleProjectException(context);
+        }
+        else
+        {
+            HandleUnknowException(context);
+        }
+    }
 
-
+    public void HandleProjectException(ExceptionContext context)
+    {
+        if(context.Exception is ErrorOnValidationException castExceptionErrorOnValidation)
+        {
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(new ErrorResponse(castExceptionErrorOnValidation.ErrorMessages));
+        }
+    }
+    public void HandleUnknowException(ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(new ErrorResponse("Erro interno"));
     }
 }
