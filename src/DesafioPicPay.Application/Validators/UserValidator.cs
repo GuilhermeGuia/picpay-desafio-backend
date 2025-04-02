2b766207c5e9946b4ea4647b2e1ed1d68f052ae2
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-02 16:24:04 -0400

    feat: adding user validator

---
+++ src/DesafioPicPay.Application/Validators/UserValidator.cs
@@ -0,0 +1,29 @@
+﻿using DesafioPicPay.Application.Services.User.Dto;
+using DesafioPicPay.Application.Validators.Base;
+using DesafioPicPay.Exception;
+using DesafioPicPay.Exception.ExceptionBase;
+
+namespace DesafioPicPay.Application.Validators;
+
+public class UserValidator : Validator
+{
+    public void CreateUserValidation(CreateUserInput input)
+    {
+        if (string.IsNullOrWhiteSpace(input.FullName))
+            AddErros(ResourceMessageExceptions.NAME_EMPTY);
+
+        if (string.IsNullOrWhiteSpace(input.Email))
+            AddErros(ResourceMessageExceptions.EMAIL_EMPTY);
+
+        if (string.IsNullOrWhiteSpace(input.Cpf))
+            AddErros(ResourceMessageExceptions.CPF_EMPTY);
+
+        if (string.IsNullOrWhiteSpace(input.Password))
+            AddErros(ResourceMessageExceptions.PASSWORD_EMPTY);
+
+        if (input.Password.Count() < 6)
+            AddErros(ResourceMessageExceptions.PASSWORD_INVALID);
+
+        ThrowIfInvalid<ErrorOnValidationException>();
+    }
+}
