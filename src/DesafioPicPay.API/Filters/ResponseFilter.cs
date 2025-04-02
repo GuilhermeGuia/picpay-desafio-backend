374d4d52d84fe022fc8ced81ab1e6a5f2089511c
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-02 16:25:24 -0400

    feat: adding global handler api responses

---
+++ src/DesafioPicPay.API/Filters/ResponseFilter.cs
@@ -0,0 +1,27 @@
+﻿using DesafioPicPay.API.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DesafioPicPay.API.Filters;
+
+public class ResponseFilter : IActionFilter
+{
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        if (context.Result is ObjectResult objectResult)
+        {
+            if (objectResult.Value == null)
+                context.Result = new ObjectResult(new ResponseApi<string>(string.Empty));
+            else
+            {
+                var responseType = typeof(ResponseApi<>).MakeGenericType(objectResult.Value.GetType());
+                var responseInstance = Activator.CreateInstance(responseType, objectResult.Value);
+                context.Result = new ObjectResult(responseInstance);
+            }
+        }
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+    }
+}
