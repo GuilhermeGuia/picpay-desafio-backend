04c0d92d0c0ffaee06787fec96594b0fb0786f8b
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-02 16:21:32 -0400

    feat: adding dbcontext

---
+++ src/DesafioPicPay.Infrastructure/DataAccess/DesafioPicPayDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DesafioPicPay.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioPicPay.Infrastructure.DataAccess;
+
+public class DesafioPicPayDbContext : DbContext
+{
+    public DesafioPicPayDbContext(DbContextOptions<DesafioPicPayDbContext> options) : base(options) { }
+
+    #region DBSETS
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<Transfer> Transfer { get; set; }
+        public virtual DbSet<Account> Account { get; set; }
+    #endregion
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DesafioPicPayDbContext).Assembly);
+    }
+}
