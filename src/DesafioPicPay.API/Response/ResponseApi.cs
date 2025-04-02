54b5619e39748c8c0f1ef61bdb4776fd3b23c59c
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-02 16:25:11 -0400

    feat: adding custom response api

---
+++ src/DesafioPicPay.API/Response/ResponseApi.cs
@@ -0,0 +1,18 @@
+﻿namespace DesafioPicPay.API.Response;
+
+public class ResponseApi<T>
+{
+    public T? Result { get; set; }
+    public bool Success { get; set; }
+    public IList<string>? Errors { get; set; }
+    public ResponseApi(T result)
+    {
+        Success = true;
+        Result = result;
+    }
+    public ResponseApi(IList<string> errors)
+    {
+        Errors = errors;
+        Success = false;
+    }
+}
