a4b87b313a6e643f83bd2729e5cf921185bdf7cf
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-02 16:22:34 -0400

    feat: adding dependecy injection infrastructure project

---
+++ src/DesafioPicPay.Infrastructure/DepedencyInjectionExtesion.cs
@@ -0,0 +1,39 @@
+﻿using DesafioPicPay.Domain.Crypto;
+using DesafioPicPay.Domain.Repositories;
+using DesafioPicPay.Infrastructure.Crypto;
+using DesafioPicPay.Infrastructure.DataAccess;
+using DesafioPicPay.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DesafioPicPay.Infrastructure;
+
+public static class DepedencyInjectionExtesion
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddDbContext(services, configuration);
+        AddCrypto(services);
+        AddRepositories(services);
+    }
+    public static void AddCrypto(IServiceCollection services)
+    {
+        services.AddScoped<IPasswordEncryptor, PasswordEncryptor>();
+    }
+    public static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Default");
+
+        services.AddDbContext<DesafioPicPayDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+    }
+    public static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+
+}
