6c461b4e1686fc5f6691fa052db52b4e1a5e35f7
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-02 16:24:56 -0400

    feat: adding dependecy injection application

---
+++ src/DesafioPicPay.Application/DepedencyInjectionExtesion.cs
@@ -0,0 +1,31 @@
+﻿using DesafioPicPay.Application.Services.User;
+using DesafioPicPay.Application.Services.User.Dto;
+using DesafioPicPay.Application.Validators;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DesafioPicPay.Application;
+
+public static class DepedencyInjectionExtesion
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        AddMappers(services);
+        AddServices(services);
+        AddValidators(services);
+    }
+    public static void AddServices(IServiceCollection services)
+    {
+        services.AddScoped<IUserAppService, UserAppService>();
+    }
+    public static void AddMappers(IServiceCollection services)
+    {
+        services.AddScoped(option => new AutoMapper.MapperConfiguration(options =>
+        {
+            options.AddProfile<UserMapProfile>();
+        }).CreateMapper());
+    }
+    public static void AddValidators(IServiceCollection services)
+    {
+        services.AddScoped<UserValidator>();
+    }
+}
