4e643ccc2523702096a896d3500c69036351012d
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-02 16:23:56 -0400

    feat: adding base validator

---
+++ src/DesafioPicPay.Application/Validators/Base/Validator.cs
@@ -0,0 +1,20 @@
+﻿
+using DesafioPicPay.Exception.ExceptionBase;
+namespace DesafioPicPay.Application.Validators.Base;
+
+public abstract class Validator
+{
+    private readonly List<string> _errors = [];
+    public IReadOnlyCollection<string> Errors => _errors;
+    protected void AddErros(string error)
+    {
+        _errors.Add(error);
+    }
+    public bool IsValid() => _errors.Count == 0;
+
+    public void ThrowIfInvalid<TException>() where TException : DesafioPicPayException
+    {
+        if (!IsValid())
+            throw (TException)Activator.CreateInstance(typeof(TException), _errors)!;
+    }
+}
