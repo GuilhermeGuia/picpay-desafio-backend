0b83948f4731e834666f901feceb32cb00f7d042
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-02 16:25:44 -0400

    feat: adding global handler custom exceptions response

---
+++ src/DesafioPicPay.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,29 @@
+﻿using DesafioPicPay.API.Response;
+using DesafioPicPay.Exception.ExceptionBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DesafioPicPay.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if(context.Exception is DesafioPicPayException castDesafioPicPayException)
+            HandleProjectException(castDesafioPicPayException, context);
+        else
+            HandleUnknowException(context);
+    }
+
+    public void HandleProjectException(DesafioPicPayException exception, ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = (int) exception.GetStatusCode();
+        context.Result = new ObjectResult(new ResponseApi<string>(exception.GetErrorMessages()));
+    }
+
+    public void HandleUnknowException(ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(new ResponseApi<string>(["Erro interno"]));
+    }
+}
