bd82bb9b0848dd459eee5f69e963ac8e361c3f48
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-02 16:17:14 -0400

    initial commit

---
+++ src/DesafioPicPay.API/Program.cs
@@ -0,0 +1,37 @@
+using DesafioPicPay.API.Filters;
+using DesafioPicPay.Application;
+using DesafioPicPay.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMvc(options =>
+{
+    options.Filters.Add(typeof(ExceptionFilter));
+    options.Filters.Add(typeof(ResponseFilter));
+});
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
