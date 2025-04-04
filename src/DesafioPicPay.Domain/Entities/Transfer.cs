aaba43ef4636222877ce66550fbf89489cbb141d
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-04 08:15:21 -0400

    feat: update transfer model

--- src/DesafioPicPay.Domain/Entities/Transfer.cs
+++ src/DesafioPicPay.Domain/Entities/Transfer.cs
@@ -5,8 +5,8 @@ namespace DesafioPicPay.Domain.Entities;
 public class Transfer
 {
     public Guid Id { get; set; }
-    public long PayerId { get; set; }
-    public long Payee { get; set; }
+    public long SenderId { get; set; }
+    public long ReciverId { get; set; }
     public double TransferAmount { get; set; }
     public bool Active { get; set; } = true;
     public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
