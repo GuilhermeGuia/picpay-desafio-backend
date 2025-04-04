7229d9d4812aa4eb3d2c6c0c527318a24114b427
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-04 15:18:38 -0400

    feat: updating constructor transfer

--- src/DesafioPicPay.Domain/Entities/Transfer.cs
+++ src/DesafioPicPay.Domain/Entities/Transfer.cs
@@ -10,4 +10,12 @@ public class Transfer
     public double TransferAmount { get; set; }
     public bool Active { get; set; } = true;
     public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+    public Transfer(long senderId, long reciverId, double transferAmount)
+    {
+        SenderId = senderId;
+        ReciverId = reciverId;
+        TransferAmount = TransferAmount;
+        Active = true;
+    }
 }
