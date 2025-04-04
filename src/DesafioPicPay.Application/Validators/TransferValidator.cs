d7a1e12a9def3f3bdc52ba237ef821d4a671fb1c
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-04 15:18:54 -0400

    feat: updating transfer validator

---
+++ src/DesafioPicPay.Application/Validators/TransferValidator.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPicPay.Application.Services.Transfer.Dto;
+using DesafioPicPay.Application.Validators.Base;
+using DesafioPicPay.Exception;
+using DesafioPicPay.Exception.ExceptionBase;
+
+namespace DesafioPicPay.Application.Validators;
+
+public class TransferValidator : Validator
+{
+    public void SendTransferValidation(Domain.Entities.User sender, TransferInput input)
+    {
+        if (sender.Balance < input.Value)
+            AddErros(ResourceMessageExceptions.INSUFFICIENT_BALANCE);
+
+        ThrowIfInvalid<ErrorOnValidationException>();
+    }
+}   
