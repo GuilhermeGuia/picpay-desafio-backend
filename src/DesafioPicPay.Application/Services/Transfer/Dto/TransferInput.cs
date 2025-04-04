ae2134c90b20ba5faf9f616e184cc7fa600e78bd
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-04 08:28:58 -0400

    feat: adding transfer dto

---
+++ src/DesafioPicPay.Application/Services/Transfer/Dto/TransferInput.cs
@@ -0,0 +1,8 @@
+﻿namespace DesafioPicPay.Application.Services.Transfer.Dto;
+
+public class TransferInput
+{
+    public long Value { get; set; }
+    public long SenderId { get; set; }
+    public long ReceiverId { get; set; }
+}
