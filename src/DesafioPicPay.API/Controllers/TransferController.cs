1645922b70461e2ea6b5712f475de4209c322e19
Guilherme Guia <guilhermeguia007@gmail.com>
2025-04-04 15:19:14 -0400

    feat: adding transfer endpoint

---
+++ src/DesafioPicPay.API/Controllers/TransferController.cs
@@ -0,0 +1,23 @@
+﻿using DesafioPicPay.Application.Services.Transfer;
+using DesafioPicPay.Application.Services.Transfer.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioPicPay.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class TransferController : ControllerBase
+{
+
+    [HttpPost]
+    [ProducesResponseType(typeof(TransferInput), StatusCodes.Status201Created)]
+    public async Task<IActionResult> Transfer(
+    [FromServices] ITransferAppService service,
+    [FromBody] TransferInput input
+)
+    {
+        var response = await service.Transfer(input);
+
+        return Created(string.Empty, response);
+    }
+}
